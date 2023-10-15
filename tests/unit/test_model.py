428e0ac90876e164b44bbf25531725ba67c541d4
Hussein Soliman <127189589+HusseinnSolimann@users.noreply.github.com>
2023-10-15 14:54:48 +0200

    Added country in backend

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -5,7 +5,7 @@ def test_create_account():
     """
     GIVEN a Account model
     WHEN a new Account is created
-    THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
+    THEN check the name, account_number, balance, currency, country, status and created_at fields are defined correctly
     """
     account = Account('John Doe', '€')
     assert account.name == 'John Doe'
